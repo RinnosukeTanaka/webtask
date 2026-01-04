dc96004d9c941403f3943bf520f6f5c73f63632c
Rin <chrn24088@g.nihon-u.ac.jp>
2026-01-05 01:05:25 +0900

    App.jsxを変更

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
 import { useState, useEffect } from 'react'
 import { MapContainer, TileLayer, Marker } from "react-leaflet";
+import L from "leaflet"; 
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png"; 
+import markerIcon from "leaflet/dist/images/marker-icon.png"; 
+import markerShadow from "leaflet/dist/images/marker-shadow.png"; 
+
+delete L.Icon.Default.prototype._getIconUrl; L.Icon.Default.mergeOptions({ iconRetinaUrl: markerIcon2x, iconUrl: markerIcon, shadowUrl: markerShadow, });
+
 const weatherBackgrounds = {
   Clear: "/images/clear.jpg", 
   Clouds: "/images/clouds.jpg", 
